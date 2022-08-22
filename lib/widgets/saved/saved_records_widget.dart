36771cc2831da416313b01cb99fed56ab54c7934
jgbisuna <51989563+jose-bamboo@users.noreply.github.com>
2022-08-22 16:47:59 +0800

    CL-32: Added empty state for saved records

--- lib/widgets/saved/saved_records_widget.dart
+++ lib/widgets/saved/saved_records_widget.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tmdbflutter/bloc/watch_later/watch_later_cubit.dart';
+import 'package:tmdbflutter/library/cubit.dart';
+import 'package:tmdbflutter/styles/styles.dart';
 import 'package:tmdbflutter/widgets/saved/saved_actors_page.dart';
 import 'package:tmdbflutter/widgets/saved/saved_movies_widget.dart';
 import 'package:tmdbflutter/widgets/saved/saved_tv_shows_widget.dart';
@@ -11,6 +13,45 @@ class SavedRecordsWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var hasRecords = false;
+    final moviesBloc = context.watch<MoviesWatchLaterCubit>();
+    final actorsBloc = context.watch<SavedActorsCubit>();
+    final tvBloc = context.watch<TvWatchLaterCubit>();
+    final blocs = <LocalStorageCubit>[
+      moviesBloc,
+      actorsBloc,
+      tvBloc,
+    ];
+    for (var bloc in blocs) {
+      if (bloc.state.length > 0) {
+        hasRecords = true;
+        break;
+      }
+    }
+
+    if (hasRecords == false) {
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.bookmark,
+              color: Colors.white.withOpacity(.5),
+              size: 100,
+            ),
+            Text(
+              'Browse through the records and tap\nthe icon to save it for later.',
+              textAlign: TextAlign.center,
+              style: Styles.mReg.copyWith(
+                fontSize: 18,
+                color: Colors.white.withOpacity(.5),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
     return ListView(
       padding: EdgeInsets.symmetric(
         horizontal: 5,
