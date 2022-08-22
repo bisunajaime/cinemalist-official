e3529d622f3ca143f43362e2d3fd195751f75533
jgbisuna <51989563+jose-bamboo@users.noreply.github.com>
2022-08-22 16:06:01 +0800

    CL-6: added removing of saved item individually for saved movies and tv shows

--- lib/widgets/saved/saved_tv_shows_widget.dart
+++ lib/widgets/saved/saved_tv_shows_widget.dart
@@ -4,7 +4,10 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:tmdbflutter/bloc/watch_later/watch_later_cubit.dart';
 import 'package:tmdbflutter/styles/styles.dart';
+import 'package:tmdbflutter/utils/delayed_runner.dart';
 import 'package:tmdbflutter/views/tvshow_page.dart';
+
+final _runner = DelayedRunner(milliseconds: 250);
 
 class SavedTvShowsWidget extends StatelessWidget {
   const SavedTvShowsWidget({Key? key}) : super(key: key);
@@ -33,50 +36,81 @@ class SavedTvShowsWidget extends StatelessWidget {
                 ),
               ),
             ),
-            child: AspectRatio(
-              aspectRatio: 2 / 3,
-              child: Container(
-                margin: EdgeInsets.all(5),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(1),
-                  color: Colors.grey,
-                ),
-                child: Stack(
-                  children: <Widget>[
-                    CachedNetworkImage(
-                      imageUrl:
-                          'https://image.tmdb.org/t/p/w500${savedTVShows[i].posterPath}',
-                      cacheManager: DefaultCacheManager(),
-                      fit: BoxFit.cover,
-                      fadeInCurve: Curves.ease,
-                      fadeInDuration: Duration(milliseconds: 250),
-                      fadeOutDuration: Duration(milliseconds: 250),
-                      fadeOutCurve: Curves.ease,
-                      height: double.infinity,
-                      width: double.infinity,
+            child: Stack(
+              children: [
+                AspectRatio(
+                  aspectRatio: 2 / 3,
+                  child: Container(
+                    margin: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(1),
+                      color: Colors.grey,
                     ),
-                    Positioned(
-                      bottom: 2,
-                      left: 2,
-                      child: Row(
-                        children: <Widget>[
-                          Icon(
-                            Icons.star,
-                            color: Colors.yellow,
-                            size: 10,
+                    child: Stack(
+                      children: <Widget>[
+                        CachedNetworkImage(
+                          imageUrl:
+                              'https://image.tmdb.org/t/p/w500${savedTVShows[i].posterPath}',
+                          cacheManager: DefaultCacheManager(),
+                          fit: BoxFit.cover,
+                          fadeInCurve: Curves.ease,
+                          fadeInDuration: Duration(milliseconds: 250),
+                          fadeOutDuration: Duration(milliseconds: 250),
+                          fadeOutCurve: Curves.ease,
+                          height: double.infinity,
+                          width: double.infinity,
+                        ),
+                        Positioned(
+                          bottom: 2,
+                          left: 2,
+                          child: Row(
+                            children: <Widget>[
+                              Icon(
+                                Icons.star,
+                                color: Colors.yellow,
+                                size: 10,
+                              ),
+                              Text(
+                                savedTVShows[i].voteAverage.toString(),
+                                style: Styles.mBold.copyWith(
+                                  fontSize: 10,
+                                ),
+                              ),
+                            ],
                           ),
-                          Text(
-                            savedTVShows[i].voteAverage.toString(),
-                            style: Styles.mBold.copyWith(
-                              fontSize: 10,
-                            ),
-                          ),
-                        ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  right: 0,
+                  child: GestureDetector(
+                    onTap: () {
+                      _runner.run(() {
+                        cubit.save(savedTVShows[i]);
+                      });
+                    },
+                    child: Container(
+                      padding: EdgeInsets.all(4),
+                      margin: EdgeInsets.only(
+                        right: 8,
+                        bottom: 8,
+                      ),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.pinkAccent,
+                      ),
+                      child: Icon(
+                        Icons.delete,
+                        color: Colors.white,
+                        size: 20,
                       ),
                     ),
-                  ],
-                ),
-              ),
+                  ),
+                )
+              ],
             ),
           );
         },
