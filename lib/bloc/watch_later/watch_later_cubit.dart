c8df092f0c11b0f0939b57e3f73a920992829507
jgbisuna <51989563+jose-bamboo@users.noreply.github.com>
2022-08-22 13:19:18 +0800

    CL-6: adde watch later cubit

---
+++ lib/bloc/watch_later/watch_later_cubit.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tmdbflutter/barrels/models.dart';
+
+class MoviesWatchLaterCubit extends Cubit<List<GenericMoviesModel>> {
+  MoviesWatchLaterCubit() : super([]);
+}
