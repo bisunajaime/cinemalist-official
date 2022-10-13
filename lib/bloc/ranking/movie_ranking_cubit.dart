23762a73a01a19a997e023d9bfb29422d0a9e33c
Jose Jaime Bisuña <jaimebisona@gmail.com>
2022-10-13 19:50:47 +0800

    CL-58: Renamed ranking_cubit to movie_ranking_cubit

--- lib/bloc/ranking/movie_ranking_cubit.dart
+++ lib/bloc/ranking/movie_ranking_cubit.dart
@@ -1,68 +1,8 @@
 import 'dart:convert';
 
-import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:tmdbflutter/barrels/models.dart';
+import 'package:tmdbflutter/models/ranking_model.dart';
 import 'package:tmdbflutter/repository/localstorage_repository/localstorage_repository.dart';
-
-class RankingModel extends Equatable {
-  final int id;
-  final String? photoUrl, title;
-  // final RankingType type;
-
-  static Map<String, RankingType> _rankingTypeMap = {
-    'movies': RankingType.movies,
-    'tvShows': RankingType.tvShows,
-    'actors': RankingType.actors,
-  };
-
-  static Map<RankingType, String> _rankingTypeToString = {
-    RankingType.movies: 'movies',
-    RankingType.tvShows: 'tvShows',
-    RankingType.actors: 'actors',
-  };
-
-  RankingModel(
-    this.id,
-    this.photoUrl,
-    this.title,
-    // this.type,
-  );
-
-  @override
-  List<Object?> get props => [id];
-
-  factory RankingModel.fromJson(Map<String, dynamic> json) {
-    return RankingModel(
-      json['id'],
-      json['photoUrl'],
-      json['title'],
-      // _rankingTypeMap[json['type']]!,
-    );
-  }
-
-  factory RankingModel.fromGenericMovieModel(GenericMoviesModel model) {
-    return RankingModel(
-      model.id!,
-      model.posterPath ?? '',
-      model.title ?? 'No title',
-      // RankingType.movies,
-    );
-  }
-
-  Map<String, dynamic> toJson() => {
-        'id': id,
-        'photoUrl': photoUrl,
-        'title': title,
-        // 'type': _rankingTypeToString[type],
-      };
-}
-
-enum RankingType {
-  movies,
-  tvShows,
-  actors,
-}
 
 final _initialMovieRankingState = <String, List<RankingModel>>{
   's': [],
@@ -79,6 +19,14 @@ class MovieRankingCubit extends Cubit<Map<String, List<RankingModel>>> {
   MovieRankingCubit() : super(_initialMovieRankingState) {
     localStorageRepository = FileLocalStorageRepository(fileName);
     initialLoad();
+  }
+
+  int get rankedMoviesCount {
+    var count = 0;
+    state.entries.forEach((element) {
+      count += element.value.length;
+    });
+    return count;
   }
 
   void setIsLoading(bool loading) {
@@ -111,12 +59,17 @@ class MovieRankingCubit extends Cubit<Map<String, List<RankingModel>>> {
   }
 
   Future<bool> saveMovie(String letter, RankingModel movie) async {
-    if (MovieRankingHelper.movieAlreadyRanked(movie.id, state)) {
+    if (MovieRankingHelper.alreadyRanked(movie.id, state) &&
+        MovieRankingHelper.alreadyRankedInLetter(movie.id, letter, state)) {
       return false;
     }
     final rankings = state[letter];
     if (rankings?.contains(movie) == true) {
       return false;
+    }
+    final letterOfMovie = MovieRankingHelper.findLetterOfModel(state, movie);
+    if (letterOfMovie != null) {
+      state[letterOfMovie]?.remove(movie);
     }
     final stateCopy = {...state};
     stateCopy[letter]!.add(movie);
@@ -145,39 +98,10 @@ class MovieRankingCubit extends Cubit<Map<String, List<RankingModel>>> {
     final rankingData = stateCopy[letter];
     final movedMovie = rankingData![oldIndex];
     rankingData.removeAt(oldIndex);
-    rankingData.insert(newIndex, movedMovie);
+    final nIndex = newIndex > rankingData.length ? newIndex - 1 : newIndex;
+    rankingData.insert(nIndex, movedMovie);
     stateCopy[letter] = rankingData;
     await localStorageRepository.save(MovieRankingHelper.encodeData(stateCopy));
     emit(stateCopy);
-  }
-}
-
-class MovieRankingHelper {
-  static Map<String, List<RankingModel>> decodeRawData(String json) {
-    final decodedJson = jsonDecode(json) as Map;
-    return decodedJson.map(
-      (key, value) => MapEntry(key as String,
-          (value as List).map((e) => RankingModel.fromJson(e)).toList()),
-    );
-  }
-
-  static String encodeData(Map<String, List<RankingModel>> data) {
-    final mappedData = data.map(
-        (key, value) => MapEntry(key, value.map((e) => e.toJson()).toList()));
-    return jsonEncode(mappedData);
-  }
-
-  static bool movieAlreadyRanked(
-    int movieId,
-    Map<String, List<RankingModel>> data,
-  ) {
-    final keys = data.keys;
-    for (var k in keys) {
-      final idList = data[k]!.map((e) => e.id);
-      if (idList.contains(movieId)) {
-        return true;
-      }
-    }
-    return false;
   }
 }
